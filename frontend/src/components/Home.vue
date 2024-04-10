7cf6db9e4d3c21d26eacc921dc9a9d0ca9a9347f
KonstiDoll <konstantin.doll@urban-codesign.com>
2024-04-10 23:00:10 +0200

    add threejs scene for viewing

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="mainContainer" class="flex flex-row h-full w-full p-2">
-        <div id='sidebar' class="flex flex-col col h-full bg-slate-200 rounded-xl overflow-hidden p-2 space-y-4">
+    <div id="mainContainer" class="relative flex flex-row h-full w-full p-2">
+        <div id='sidebar' class="flex flex-col col h-full bg-slate-200 rounded-xl overflow-hidden p-2 space-y-4 shrink-0">
             <div class="text-slate-800 font-semibold text-lg text-center p-2">Image-Opencv-Plotter.tech</div>
             <button class="w-fit mx-auto rounded-xl p-2 bg-slate-400 hover:bg-slate-500 active:bg-slate-600 ">
                 <PhotoIcon class="h-16" />
@@ -13,7 +13,7 @@
         <div id="content" class="relative flex flex-col p-6 w-full bg-slate-200/0 rounded-xl">
 
             <div
-                class="relative flex flex-col justify-center items-center text-center w-full h-28 p-6 m-6 hover:bg-slate-200/80  outline-dashed outline-2 rounded-lg outline-slate-300 hover:outline-offset-4">
+                class="relative flex flex-col justify-center items-center text-center w-full !h-28 p-6 hover:bg-slate-200/80  outline-dashed outline-2 rounded-lg outline-slate-300 hover:outline-offset-4">
                 <button>Datei
                     hochladen</button>
                 <label class="text-slate-600">oder hier ablegen</label>
@@ -28,23 +28,22 @@
                     <div>Features extrahieren</div>
                 </button>
             </div>
-
-            <div id="svgcontainer"></div>
-
+            <ThreejsScene />
         </div>
+        
     </div>
 </template>
 
 <script setup lang="ts">
 import { CubeIcon, PhotoIcon } from '@heroicons/vue/24/solid'
 import { ref } from 'vue';
+import ThreejsScene from './ThreejsScene.vue';
 // import { uploadImage } from '../utils/backend_services';
 const loadedFile = ref<File>()
 const uploadedFile = ref<File[]>([])
 const handleImageUpload = (e: any) => {
     uploadedFile.value = e.target.files;
-    if(uploadedFile.value.length > 0)
-        {loadedFile.value = uploadedFile.value[0];}
+    if (uploadedFile.value.length > 0) { loadedFile.value = uploadedFile.value[0]; }
     loadSVG();
 }
 
@@ -61,19 +60,19 @@ function loadSVG() {
             const paths = svg.querySelectorAll('path');
             // console.log(paths);
             paths.forEach((path: any) => {
-                const gPath = {d:path.getAttribute('d')}
+                const gPath = { d: path.getAttribute('d') }
                 const gParts = gPath.d.split(' ');
                 const gcode = createGcodeFromPath_M(gParts);
                 console.log(gcode)
             });
             const rects = svg.querySelectorAll('rect');
             console.log(rects);
-    
+
         })
         .catch(error => console.error('Error loading SVG:', error));
 }
 
-const createGcodeFromPath_M = (pathParts: string[]):string => {
+const createGcodeFromPath_M = (pathParts: string[]): string => {
     // create Gcode from path
     const down = ref(false);
     const first = ref(true);
@@ -81,30 +80,30 @@ const createGcodeFromPath_M = (pathParts: string[]):string => {
     const travelingSpeed = 'F6000';
     let gCode = '';
     pathParts.forEach((part: string) => {
-        if(part === 'M') {
+        if (part === 'M') {
             down.value = true;
         }
-        else if(down.value) {
-            if(part.split(',').length === 2) {
-                const {x,y} = {x: part.split(',')[0], y: part.split(',')[1]};
+        else if (down.value) {
+            if (part.split(',').length === 2) {
+                const { x, y } = { x: part.split(',')[0], y: part.split(',')[1] };
                 const GIndicator = first.value ? 'G0' : 'G1';
                 const speed = first.value ? travelingSpeed : drawingSpeed;
-                const gCodeLine = GIndicator + ' X' + x + ' Y' + y + ' ' + speed +'\n';
-               
+                const gCodeLine = GIndicator + ' X' + x + ' Y' + y + ' ' + speed + '\n';
+
                 // console.log(gCodeLine);
                 gCode += gCodeLine;
-                if(first.value){
+                if (first.value) {
                     const debugLine = ';start drawing here' + '\n';
                     gCode += debugLine;
                 }
                 first.value = false;
             }
-            else{
+            else {
                 console.log('unsuspected part:', part);
                 debugger
             }
         }
-       
+
     })
 
     return gCode
