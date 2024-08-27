31f10bbcdd08cc5059bccade963ad1b6a8771b81
KonstiDoll <konstantin.doll@urban-codesign.com>
2024-08-27 21:26:21 +0200

    unchecked changes

--- frontend/src/components/ThreejsScene.vue
+++ frontend/src/components/ThreejsScene.vue
@@ -8,15 +8,21 @@ import { ref, reactive, onMounted, watch, markRaw, shallowRef } from 'vue';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as THREE from 'three';
 import { useMainStore } from '../store';
+import * as TWEEN from '@tweenjs/tween.js';
 const store = useMainStore();
 const addableObject = shallowRef<THREE.Group>();
 watch(() => store.lineGeometry, (newVal) => {
     if (newVal) {
         addableObject.value = markRaw(newVal);
-        addableObject.value.rotateX(Math.PI);
-        scene.add(addableObject.value);
+        // addableObject.value.rotateX(Math.PI);
+        const newGroup = new THREE.Group();
+        scene.add(newGroup);
+        console.log(addableObject.value.children.length)
+        addObjectsAsync();
+
     }
 })
+
 const threejsMap = ref<HTMLElement>();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 20, 10000);
@@ -25,7 +31,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 const domElement = renderer.domElement;
 camera.position.set(0, 0, 5000);
+camera.position.set(229.14401307546788, -538.4501816598943, 332)
 const controller = new OrbitControls(camera, domElement);
+controller.target.set(228.73873934507935, -545.7206095178788, 3.693646875546118e-14)
+
 controller.enableDamping = true;
 controller.update();
 const geometry = new THREE.BoxGeometry(100, 100, 100);
@@ -33,8 +42,37 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.background = new THREE.Color(0x051935);
 // scene.add(cube);
-
+const controllerTarget = controller.target;
+const controllerGoalTarget = new THREE.Vector3(1500, -538.4501816598943, 0);
+const tween = new TWEEN.Tween(controllerTarget)  // Define the object and property to tween
+    .to(controllerGoalTarget, 20000)                       // Define the target value and duration
+    .easing(TWEEN.Easing.Cubic.InOut)         // Define the easing function
+    .onUpdate(() => {
+        controller.target = controllerTarget
+        controller.update();
+    })  // Update the scene during tween
+// .start();
+const camPosition = camera.position;
+const camGoalPosition = new THREE.Vector3(1500, -538.4501816598943, 500);
+const tween1 = new TWEEN.Tween(camPosition)  // Define the object and property to tween
+    .to(camGoalPosition, 20000)                       // Define the target value and duration
+    .easing(TWEEN.Easing.Cubic.InOut)         // Define the easing function
+    .onUpdate(() => {
+        camera.position.set(camPosition.x, camPosition.y, camPosition.z);
+    })  // Update the scene during tween
+// .start();
+let camPosition1 = camera.position
+const camGoalPosition1 = new THREE.Vector3(1500, -538.4501816598943, 3600);
+const tween2 = new TWEEN.Tween(camPosition1)  // Define the object and property to tween
+    .to(camGoalPosition1, 20000)                       // Define the target value and duration
+    .easing(TWEEN.Easing.Cubic.InOut)         // Define the easing function
+    .onUpdate(() => {
+        camera.position.set(camPosition1.x, camPosition1.y, camPosition1.z);
+        controller.update();
+    })  // Update the scene during tween
+// .start();
 onMounted(() => {
+
     threejsMap.value?.appendChild(domElement);
     window.addEventListener("resize", setSize);
     setSize();
@@ -50,11 +88,51 @@ const setSize = () => {
     renderer.setSize(divSize.value.width, divSize.value.height);
     renderer.setPixelRatio(window.devicePixelRatio);
 };
+let enableNormalRender = true;
+let index = 0;
+let first = true;
+let second = true;
+const addObjectsAsync = () => {
+    enableNormalRender = false;
 
+    const length = addableObject.value?.children?.length;
+    if (index < length) {
+        const child = addableObject.value.children[(length - 1) - index];
+        child.rotateX(Math.PI);
+        scene.add(child);
+        index++;
+        console.log(index)
+        renderer.render(scene, camera);
+        TWEEN.update();
+        if (index > 300 && first) {
+            tween.start();
+            tween1.start();
+            first = false;
+        }
+        if (index > 1600 && second) {
+            camPosition1 = camera.position;
+            tween2.start();
+            second = false;
+        }
+        requestAnimationFrame(addObjectsAsync);
+    } else {
+        enableNormalRender = true
+        requestAnimationFrame(animate);
+    }
+
+};
 const animate = function () {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    controller.update();
+    if (enableNormalRender) {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        controller.update();
+        console.log('target: ')
+        console.log(controller.target)
+        console.log('position: ')
+        console.log(camera.position)
+
+    }
+
 };  
 </script>
 
--- frontend/src/utils/threejs_services.ts
+++ frontend/src/utils/threejs_services.ts
@@ -3,18 +3,19 @@ import * as THREE from 'three';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 import { computed } from 'vue';
 
-export function getThreejsObjectFromSvg(svgContent: string): Promise<THREE.Group> {
+export function getThreejsObjectFromSvg(svgContent: string): THREE.Group {
     const loader = new SVGLoader();
     const svg = loader.parse(svgContent);
 
     const paths: THREE.ShapePath[] = svg.paths;
     const group = new THREE.Group();
     paths.forEach((shapePath) => {
+        console.log('shapepath: ', shapePath)
         const randomColor = Math.floor(Math.random() * 0xffffff);
         const material = new THREE.LineBasicMaterial({
             color: randomColor,
         });
-        shapePath.subPaths.forEach((subPath: THREE.Line) => {
+        shapePath.subPaths.forEach((subPath: THREE.Path) => {
             // Get points from the subPath
             // If the subPath contains curves, you might want to get more points for a smoother line
             const points: THREE.Vector2[] = subPath.getPoints();
@@ -33,20 +34,94 @@ export function getThreejsObjectFromSvg(svgContent: string): Promise<THREE.Group
                     }
                 })
                 line.userData.isClosed = isClosed.value;
+
             }
+            // if (!line.userData.isClosed) {
+            //     group.add(line);
+            // }
             group.add(line);
         });
     });
     //chech for closedPaths and create shapes from them
-    
-    const closedPaths = paths.filter((path) => path.userData.isClosed);
-    const closedShapes = createShapesForClosedPaths(closedPaths);
-    closedShapes.forEach((shape) => {
-        // group.add(shape);
-    });
 
+    // const closedPaths = paths.filter((path) => path.userData.isClosed);
+    // closedPaths.forEach((path) => {
+    //     console.log('path: ', path)
+    //     path.subPaths.forEach((subPath) => {
+    //         const curves = subPath.curves;
+    //         const infillLines = connectPointsOfCurves(curves)
+    //         infillLines.forEach((line) => {
+    //             const material = new THREE.LineBasicMaterial({
+    //                 color: 0xffffff,
+    //             });
+    //             const geometry = new THREE.BufferGeometry().setFromPoints([line.start, line.end]);
+    //             const lineMesh = new THREE.Line(geometry, material);
+    //             group.add(lineMesh);
+    //         })
+    //     });
+    // });
+    // const sortedGroup = sortLines(group);
     return group;
 }
+
+const sortLines = (group: THREE.Group) => {
+    // Function to compute distance between two points
+    const distance = (a, b) => a.distanceTo(b);
+
+    // Extract first and last points from each line
+    const endpoints = group.children.map(line => {
+        const positions = line.geometry.attributes.position.array;
+        const numPoints = positions.length / 3;
+        return {
+            start: new THREE.Vector3(positions[0], positions[1], positions[2]),
+            end: new THREE.Vector3(positions[(numPoints - 1) * 3], positions[(numPoints - 1) * 3 + 1], positions[(numPoints - 1) * 3 + 2])
+        };
+    });
+
+    let sortedLines = [];
+    let currentLineIndex = 0; // Start from the first line
+    sortedLines.push(group.children[currentLineIndex]);
+
+    // Nearest neighbor greedy algorithm
+    for (let i = 1; i < endpoints.length; i++) {
+        const lastEndpoint = endpoints[currentLineIndex].end;
+        let minDistance = Infinity;
+        let nextIndex = -1;
+
+        for (let j = 0; j < endpoints.length; j++) {
+            if (j !== currentLineIndex && !sortedLines.includes(group.children[j])) {
+                const start = endpoints[j].start;
+                const dist = distance(lastEndpoint, start);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    nextIndex = j;
+                }
+            }
+        }
+
+        if (nextIndex >= 0) {
+            sortedLines.push(group.children[nextIndex]);
+            currentLineIndex = nextIndex;
+        }
+    }
+    // Replace original group's children with sorted lines
+    group.children = sortedLines;
+}
+function connectPointsOfCurves(curves: THREE.Curve<THREE.Vector2>[]): THREE.Line3[] {
+    let points: THREE.Vector2[] = [];
+    curves.forEach(curve => {
+        points = points.concat(curve.getPoints()); // Get points from curve
+    });
+    const lines: THREE.Line3[] = [];
+    const points3D = points.map((point) => new THREE.Vector3(point.x, point.y, 0));
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i + 1; j < points.length; j++) {
+            lines.push(new THREE.Line3(points3D[i], points3D[j]));
+        }
+    }
+    return lines;
+}
+
 export function createShapesForClosedPaths(paths: THREE.ShapePath[]): THREE.Mesh[] {
 
     return paths.map((path) => {
